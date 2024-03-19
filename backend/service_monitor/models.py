80d60d5f4470957d9cc05814145855a98a798dcf
Keegan White <keeganthomaswhite@gmail.com>
2024-03-19 11:55:08 +0200

    created service monitoring model and view

--- backend/service_monitor/models.py
+++ backend/service_monitor/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
+
 # Create your models here.
+class Service(models.Model):
+    SERVICE_TYPES = (
+        ('utility', 'utility'),
+        ('entertainment', 'entertainment'),
+        ('games', 'games'),
+        ('education', 'education')
+    )
+
+    API_LOCATIONS = (
+        ('cloud', 'cloud'),
+        ('local', 'local')
+    )
+
+    url = models.URLField(max_length=100)
+    name = models.CharField(max_length=20)
+    service_type = models.CharField(max_length=20, choices=SERVICE_TYPES)
+    api_location = models.CharField(max_length=10,choices=API_LOCATIONS)
--- backend/service_monitor/views.py
+++ backend/service_monitor/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from rest_framework.views import APIView, Response
+from rest_framework import status
+from .models import Service
 
-# Create your views here.
+class ListServices(APIView):
+    def get(self, request):
+        services = Service.objects.all()
+        data = [{'name': s.name} for s in services]
+        return Response({'status': 'success', 'message': data}, status=status.HTTP_200_OK)
