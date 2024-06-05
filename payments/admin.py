adfe936d89633b49be78288c3c335c965eff21af
Erik Polzin <eriktpol@gmail.com>
2024-06-05 19:35:14 +0200

    Remove users & payments modules

--- payments/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from django.contrib import admin
-from .models import Payment
-
-@admin.register(Payment)
-class PaymentAdmin(admin.ModelAdmin):
-    list_display = ('user', 'amount', 'created_at')
--- users/models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class User(models.Model):
-    keycloak_username = models.CharField(max_length=50, unique=True)
-    wallet_address = models.CharField(max_length=50)
