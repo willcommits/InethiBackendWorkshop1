55f8e33421382a14418c1b452127ed9dc375b945
Erik Polzin <eriktpol@gmail.com>
2024-06-25 12:34:14 +0200

    Add syncunifi task

--- monitoring/tasks.py
+++ monitoring/tasks.py
@@ -2,6 +2,7 @@ from celery import shared_task
 from celery.utils.log import get_task_logger
 
 from .sync.radiusdesk import run as syncrd
+from .sync.unifi import run as syncunifi
 
 logger = get_task_logger(__name__)
 
@@ -10,3 +11,9 @@ logger = get_task_logger(__name__)
 def run_syncrd():
     logger.info("Syncing with radiusdesk")
     syncrd()
+
+
+@shared_task
+def run_syncunifi():
+    logger.info("Syncing with unifi")
+    syncunifi()
