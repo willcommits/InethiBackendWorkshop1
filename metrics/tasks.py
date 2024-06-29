b8dea61a1ad0ba8f7be5eee2579297df01d6834a
Erik Polzin <eriktpol@gmail.com>
2024-06-29 20:55:41 +0200

    Remove old node reference

--- metrics/tasks.py
+++ metrics/tasks.py
@@ -1,7 +1,8 @@
 from celery import shared_task
 from celery.utils.log import get_task_logger
 
-from .models import Node, UptimeMetric, RTTMetric
+from monitoring.models import Node
+from .models import UptimeMetric, RTTMetric
 from .ping import ping
 
 logger = get_task_logger(__name__)
@@ -12,7 +13,7 @@ def run_pings():
     for device in Node.objects.filter(ip__isnull=False):
         ping_data = ping(device.ip)
         rtt_data = ping_data.pop("rtt", None)
-        UptimeMetric.objects.create(node=device, **ping_data)
+        UptimeMetric.objects.create(mac=device.mac, **ping_data)
         if rtt_data:
-            RTTMetric.objects.create(node=device, **rtt_data)
+            RTTMetric.objects.create(mac=device.mac, **rtt_data)
         logger.info(f"PING {device.ip}")
